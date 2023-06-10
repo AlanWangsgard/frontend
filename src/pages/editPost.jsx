2d54bf1e7e3b14d5bceacd79753e0dc39522d0c5
alanwangsgard <wan20003@byui.edu>
2023-06-09 19:24:33 -0700

    fetch uses hooks instead of my other garbage

--- src/pages/editPost.jsx
+++ src/pages/editPost.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Nav from "./nav";
 import { useParams } from 'react-router-dom'
 
@@ -19,35 +20,58 @@ async function updatePost(postId){
 }
 
 
-async function getPost(postId){
-    let url = 'http://localhost:3000/posts/byId/' + postId
-        let data = await fetch(url)
-        let posts = await data.json()
+// async function getPost(postId){
+//     let url = 'http://localhost:3000/posts/byId/' + postId
+//         let data = await fetch(url)
+//         let posts = await data.json()
 
-        posts.forEach(element =>{
-            let t = document.createElement('textarea')
-            t.value = element.text
-            t.className = "text"
+//         posts.forEach(element =>{
+//             let t = document.createElement('textarea')
+//             t.value = element.text
+//             t.className = "text"
 
-            let b = document.createElement('button')
-            b.innerHTML = 'update'
-            b.onclick = () => updatePost(postId)
+//             let b = document.createElement('button')
+//             b.innerHTML = 'update'
+//             b.onclick = () => updatePost(postId)
 
-            let contain = document.querySelector('.container')
-            contain.appendChild(t)
-            contain.appendChild(b)
+//             let contain = document.querySelector('.container')
+//             contain.appendChild(t)
+//             contain.appendChild(b)
 
 
-        })
-}
+//         })
+// }
 
 export default function EditPost(){
     const {postId} = useParams()
-    getPost(postId)
+    let url = 'http://localhost:3000/posts/byId/' + postId
+    // getPost(postId)
+   
+        const [posts, setPosts] = useState([])
+
+        const fetchData = () => {
+            fetch(url)
+            .then(response => {
+                return response.json()
+            }).then(data => {
+                setPosts(data)
+            })
+        }
+    
+
+    useEffect(() => {
+        fetchData()
+    }, [])
     return(
         <>
         <Nav />
         <div className="container"></div>
+        {posts.length > 0 && (
+            <div>
+            {posts.map((post, index) => <textarea key={postId} className="text" defaultValue={post.text}></textarea>)}
+            </div>
+        )}
+        <button onClick={() => updatePost(postId)} >Update</button>
         </>
     )
 }
